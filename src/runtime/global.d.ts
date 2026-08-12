f2c0e0712eef7c428d48778ed4cb382b1d33f02d
kevin-luo <166820145@qq.com>
2026-08-12 14:50:51 +0800

    feat: expose story manifest for export tools

--- src/runtime/global.d.ts
+++ src/runtime/global.d.ts
@@ -1,8 +1,10 @@
 import type { LoopAnimationController } from './animation';
+import type { StoryManifest } from './story';
 
 declare global {
   interface Window {
     __LOOP_ANIMATION__?: LoopAnimationController;
+    __LOOP_STORY__?: StoryManifest;
   }
 }
 
